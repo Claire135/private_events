a961ac9f8f5f77419b911158f8f2323755b5a9cc
ClaireCompare <claire@markcompare.com>
2025-05-19 18:58:54 +0200

    Add Edit and Delete functionality to events - Add edit and delete links to User#show - Add delete dependency to invitations
    Add date parameters to organise events
    - Add scope to Event Model for previous and future events
    
    Refactor Event#new View
    - Refactored #new into partial

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,28 @@
 class Event < ApplicationRecord
+  scope :past, -> { where("date < ?", Date.today) }
+  scope :upcoming, -> { where("date >= ?", Date.today) }
+
   belongs_to :creator, class_name: "User"
 
-  has_many :invitations, foreign_key: "event_id"
+  has_many :invitations, foreign_key: "event_id", dependent: :destroy
   has_many :attendees, through: :invitations, source: :attendee
 
   validates :name, :date, :location, presence: true
+
+  def no_of_attendees
+    attendees.count
+  end
+
+  # as a class methods - replaced by scope - left for educational purposes
+  # def self.past
+  #  where("date < ?", Date.today)
+  # end
+
+  # def self.upcoming
+  #  where("date > ?", Date.today)
+  # end
+
+  # def self.this_month
+  #  where(date: Date.today.beginning_of_month..Date.today.end_of_month)
+  # end
 end
